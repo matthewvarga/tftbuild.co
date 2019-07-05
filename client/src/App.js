7f95341af33c7d19cb57505f23186a91cb1aca76
Matthew Varga <matthew.l.varga@gmail.com>
2019-07-05 15:41:42 -0400

    added item table

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Classes from "./components/classes/index";
+import Items from "./components/items/index";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+		<Items/>
 		<Classes/>
     </div>
   );

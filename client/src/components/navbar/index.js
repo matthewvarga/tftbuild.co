1ee47d42cc47c6d511154689db1b05449d769e66
Matthew Varga <matthew.l.varga@gmail.com>
2019-07-19 02:31:13 -0400

    added svg icons for each section to navbar

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import Hamburger from "./components/hamburger/index";
+import IconClass from "../icons/classes/index";
+import IconItems from "../icons/items/index";
+import IconPercentage from "../icons/percentage/index";
 import './index.css'
 
 class NavBar extends Component {
@@ -25,6 +28,20 @@ class NavBar extends Component {
                     <div className={"navbar-header-row" }>
                         <h1 className={"navbar-header-title"}>tftbuild</h1>
                         <Hamburger onToggle={() => this.toggle()}/>
+                    </div>
+                    <div className={"navbar-items"}>
+                        <div className={"navbar-item-row"}>
+                            <h2 className={"navbar-item-row-title"}>ITEMS</h2>
+                            <IconItems className={"navbar-item-row-icon"}/>
+                        </div>
+                        <div className={"navbar-item-row"}>
+                            <h2 className={"navbar-item-row-title"}>CLASSES</h2>
+                            <IconClass className={"navbar-item-row-icon"}/>
+                        </div>
+                        <div className={"navbar-item-row"}>
+                            <h2 className={"navbar-item-row-title"}>CHAMPIONS</h2>
+                            <IconPercentage className={"navbar-item-row-icon"}/>
+                        </div>
                     </div>
                 </div>
             </div>
